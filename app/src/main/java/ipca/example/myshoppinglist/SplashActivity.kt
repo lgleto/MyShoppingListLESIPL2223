20a189cf2549d817b57aedbbe44ecd86bc8aea9c
lourencogomes <leto.lg@gmail.com>
2022-12-13 22:37:06 +0000

    - Push Notifications

--- app/src/main/java/ipca/example/myshoppinglist/SplashActivity.kt
+++ app/src/main/java/ipca/example/myshoppinglist/SplashActivity.kt
@@ -3,9 +3,12 @@ package ipca.example.myshoppinglist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -14,6 +17,19 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            Log.d(TAG, token)
+        })
 
         lifecycleScope.launch(Dispatchers.Main) {
             delay(1000)
@@ -26,5 +42,9 @@ class SplashActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    companion object{
+        const val TAG = "SplashActivity"
     }
 }
